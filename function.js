6cb1a2b6bcceb8c819d500ef525577e8e70091a4
zilani131 <zilani.cuet@gmail.com>
2022-01-29 15:52:59 +0600

    first

---
+++ function.js
@@ -0,0 +1,78 @@
+// function zilani(){
+//     console.log("zilani is a good boy")
+// }
+// zilani();
+// console.log("ziladdbsa")
+// zilani();
+// function singarakhabo(taka){
+//     console.log("mama singara deo & tk neo",taka)
+//     var price=10;
+//     var quantity=taka/price;
+//     return quantity;
+
+// }
+// var money=250;
+// var remaintk =singarakhabo(money)
+// console.log(remaintk)
+// function addnum(num1,num2){
+//     var sum=num1+num2;
+//     return sum;
+// }
+// var firstNum,secondNum;
+// firstNum=4;
+// secondNum=6;
+// result=addnum(firstNum,secondNum)
+// console.log("sum is ",result);
+// function substract(num1,num2){
+//     var sub=num1-num2;
+//     return sub;
+// }
+// var firstNum=10,secondNum=4;
+// var result=substract(firstNum,secondNum)
+// console.log("result is " ,result)
+function mul(nu1,nu2,nu3){
+    var multiplication =nu1*nu2*nu3;
+    return multiplication;
+}
+var num1=10,num2=3,num3=6;
+var result=mul(num1,num2,num3)
+console.log("multiplication",result)
+// function division(num1,num2){
+//     var div=num1/num2;
+//     return div
+// }
+// var first=45,second=5;
+// var result=division(first,second)
+
+// console.log("division is ",result)
+// var smartphone={
+//     brand:"iphone"
+// }
+// console.log(smartphone[brand])
+// function getReminder(num1,num2){
+//     num1%num2
+// }
+// const reminder=getReminder(12,2);
+// console.log(reminder)
+// function addNumber(num1,num2)
+// {
+//     return "result is " + num1 +num2;
+// }
+// var result=addNumber(5,4);
+// console.log(result)
+// console.log(typeof result)
+var students ={
+    name :"zilani",
+    age :70
+}
+// console.log(students.name)
+// console.log(students)
+// students.name="zil"
+// console.log(students)
+// students["age"]=60;
+// console.log(students)
+var ni="name"
+
+
+students[ni]="zilaniabdul"
+console.log(students)
---
+++ multiplication-of-13.js
@@ -0,0 +1,12 @@
+function mul(x,i)
+{
+    for(i;i<11;i++){
+        var multiplication=x*i
+        console.log(y+"*"+[i]+"=",multiplication)
+        
+    }
+ 
+}
+var y=14,j=1;
+mul(y,j);
+
