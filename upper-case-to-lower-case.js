be87a7a797e07cae936c13c941583d56f523c7ab
zilani131 <zilani.cuet@gmail.com>
2022-01-29 16:08:06 +0600

    to lowercase

---
+++ upper-case-to-lower-case.js
@@ -0,0 +1,11 @@
+function uppercase(z){
+    var u=z.toUpperCase()
+    console.log(u)
+    var result=z.toLowerCase()
+    return result;
+
+}
+var x
+x="zilANi"
+var result1=uppercase(x);
+console.log("lower case form is",result1)
