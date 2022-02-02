10289410e8c1e2df5f322d398e2b71c43db0bfa4
zilani131 <zilani.cuet@gmail.com>
2022-02-02 11:06:41 +0600

    februry-2

---
+++ farhenite-to-celsius.js
@@ -0,0 +1,7 @@
+function celsius(z){
+    let cel=(z-32)*5/9;
+    return cel ;
+}
+let temp=68;
+let result=celsius(temp);
+console.log(result)
---
+++ quiz.js
@@ -0,0 +1,10 @@
+var sentence ="zilani abdul"
+var count=0;
+for(var i=0;i<sentence.length;i++){
+    var letter =sentence[i]
+    if (letter == "a")
+    {
+        count++;
+    }
+}
+console.log(count)
